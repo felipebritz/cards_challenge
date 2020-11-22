28579d6ae4c224cd88723fea9834c686ed790590
felipebritz <felipegustavobritz@gmail.com>
2020-11-22 20:51:51 -0300

    feat: implement class CardService

--- bin/src/services/card_services.dart
+++ bin/src/services/card_services.dart
@@ -7,6 +7,46 @@ class CardService {
 
   Future<List<Card>> findAll() async {
     var response = await dio.get('/cards');
-    return response.data.map((card) => Card.fromJson(card)).toList();
+
+    return (response.data as List<dynamic>)
+        .map((card) => Card.fromJson(card))
+        .toList();
+  }
+
+  Future<Card> findById(int id) async {
+    var response = await dio.get('/cards/$id');
+
+    return Card.fromJson(response.data);
+  }
+
+  Future<Card> create(String title, String content) async {
+    var response =
+        await dio.post('/cards', data: {'title': title, 'content': content});
+
+    return Card.fromJson(response.data);
+  }
+
+  Future<bool> update(int id, {String title, String content}) async {
+    var response =
+        await dio.put('/cards/$id', data: {'title': title, 'content': content});
+
+    if (response.data['id'] == id &&
+        response.data['title'] == title &&
+        response.data['content'] == content) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<bool> delete(int id) async {
+    var response = await dio.delete('/cards/$id');
+
+    try {
+      response.data as List<dynamic>;
+      return true;
+    } catch (e) {
+      return false;
+    }
   }
 }
